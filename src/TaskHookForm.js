a9aae8dd35febaa39992bbfd7c292175fc3e6a52
samet7ozturk <sametozturk973@gmail.com>
2023-11-13 17:50:26 +0300

    TaskHookForm.js create three input

--- src/TaskHookForm.js
+++ src/TaskHookForm.js
@@ -1,7 +1,84 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
 
-export default function TaskHookForm() {
+export default function TaskHookForm({ kisiler }) {
+  const emptyData = {
+    name: "",
+    description: "",
+    people: [],
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({ defaultValues: { ...emptyData }, mode: "onBlur" });
+
+  const onSubmit = (formData) => {
+    console.log("formData > ", formData);
+  };
+
   return (
-    <div>Formunuzu react-hook-form kullanarak burada oluşturun. TaskForm dosyasındaki HTML yapısını vs app.css içerisindeki classları kullanabilirsiniz.</div>
-  )
+    <form className="taskForm" onSubmit={handleSubmit(onSubmit)}>
+      <div className="form-line">
+        <label className="input-label" htmlFor="title">
+          Başlık
+          <input
+            className="input-text"
+            type="text"
+            {...register("name", {
+              required: "Başlık alanı boş bırakılamaz!",
+              minLength: {
+                value: 3,
+                message: "Başlık üç karakterden az olamaz!",
+              },
+            })}
+          ></input>
+        </label>
+        <div className="input-error">{errors?.name?.message}</div>
+      </div>
+
+      <div className="form-line">
+        <label className="input-label" htmlFor="description">
+          Açıklama
+          <input
+            className="input-textarea"
+            type="text"
+            {...register("description", {
+              required: "Açıklama alanı boş bırakılamaz!",
+              minLength: {
+                value: 10,
+                message: "Açıklama on karakterden az olamaz!",
+              },
+            })}
+          ></input>
+          <div className="input-error">{errors?.description?.message}</div>
+        </label>
+      </div>
+
+      <div className="form-line">
+        <label className="input-label">Kişiler</label>
+        <div>
+          {kisiler.map((p) => (
+            <label className="input-checkbox" key={p}>
+              <input
+                type="checkbox"
+                name="people"
+                value={p}
+                {...register("people")}
+              />
+              {p}
+              <div className="validation-error">{errors?.people?.message}</div>
+            </label>
+          ))}
+        </div>
+      </div>
+
+      <div className="form-line">
+        <button className="submit-button" type="submit" disabled={!isValid}>
+          Kaydet
+        </button>
+      </div>
+    </form>
+  );
 }
